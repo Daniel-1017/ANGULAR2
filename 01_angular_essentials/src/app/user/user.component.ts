fafe94519bd77b1f3c083487d3a098f00d49174e
Daniel-1017 <frimudaniel17@gmail.com>
2024-10-05 22:24:14 +0200

    working with outputs & emitting data

--- 01_angular_essentials/src/app/user/user.component.ts
+++ 01_angular_essentials/src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, Input, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,8 +15,10 @@ import { Component, computed, Input, input } from '@angular/core';
 })
 export class UserComponent {
   // Decorator approach
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   // Signals approach
   // avatar = input.required<string>();
@@ -20,5 +29,7 @@ export class UserComponent {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
