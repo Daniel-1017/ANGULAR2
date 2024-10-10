f5dc30867ce701b55d52bc322c944bea93680d22
Daniel-1017 <frimudaniel17@gmail.com>
2024-10-10 21:30:17 +0200

    using a service for cross-component communication

--- 03_practice/src/app/user-input/user-input.component.ts
+++ 03_practice/src/app/user-input/user-input.component.ts
@@ -1,6 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,7 +10,7 @@ import { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
+  private investmentService = inject(InvestmentService);
 
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
@@ -18,12 +18,13 @@ export class UserInputComponent {
   enteredDuration = signal('10');
 
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(),
       annualInvestment: +this.enteredAnnualInvestment(),
       expectedReturn: +this.enteredExpectedReturn(),
       duration: +this.enteredDuration(),
     });
+
     this.enteredInitialInvestment.set('0');
     this.enteredAnnualInvestment.set('0');
     this.enteredExpectedReturn.set('0');
