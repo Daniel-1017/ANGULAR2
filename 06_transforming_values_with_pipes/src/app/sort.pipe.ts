7fce85a61081348be19f090fb154e1d2bedfbab1
Daniel-1017 <frimudaniel17@gmail.com>
2024-11-01 13:59:14 +0100

    building a pipe that sorts items

---
+++ 06_transforming_values_with_pipes/src/app/sort.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort',
+  standalone: true,
+})
+export class SortPipe implements PipeTransform {
+  transform(value: string[] | number[], direction: 'asc' | 'dsc' = 'asc') {
+    const sorted = [...value];
+    sorted.sort((a, b) => {
+      if (direction === 'asc') return a > b ? 1 : -1;
+      return a > b ? -1 : 1;
+    });
+    return sorted;
+  }
+}
